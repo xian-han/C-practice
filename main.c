46191a4b74b9d2f732eb26d03442ed301e726e3f
xianhan <xianhan0421@gmail.com>
2021-06-17 12:40:58 +0800

    c-practice ch9.3.19

--- main.c
+++ main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-int idx(int arr[]);
+void square(int arr[]);
 int main(void)
 {
     int a[6]={3,6,2,5,4,1};
     int i;
     for(i=0;i<6;i++){
-        printf("%2d",a[i]);
+        printf("%3d",a[i]);
     }
     putchar('\n');
-    printf("the index of minimum of a=%2d\n",idx(a));
+    square(a);
+    //printf("the index of minimum of a=%2d\n",idx(a));
     return 0;
 }
-int idx(int arr[]){
-    int i,min_index=0,min;
-    for(i=0,min=arr[0];i<6;i++){
-        if(arr[i]<min){
-            min=arr[i];
-            min_index=i;
-        }
+void square(int arr[]){
+    int i;
+    printf("---after square()---\n");
+    for(i=0;i<6;i++){
+        arr[i]*=arr[i];
+        printf("%3d",arr[i]);
     }
-    return min_index+1;
+    putchar('\n');
 }
 
 
