c6217c42d71da555917a2c11fafc53700eda6e1b
xianhan <xianhan0421@gmail.com>
2021-06-17 11:54:46 +0800

    c-practice ch9.2.15

--- main.c
+++ main.c
@@ -3,78 +3,17 @@
 
 int main(void)
 {
-    //C語言練習9.2.14
-    float table[3][4]={{18.2,17.3,15.0,13.4},{23.8,25.1,20.6,17.8},{20.6,21.5,18.4,15.7}};
-    int i,j;
-    //印出陣列內容
-    for(i=0;i<3;i++){
-        for(j=0;j<4;j++){
-            printf("%5.1f",table[i][j]);
-        }
-        putchar('\n');
-    }
-    printf("------------------\n");
-
-    //每日平均溫度
-    float avr_temp[4]={0},total_temp;
-    for(j=0;j<4;j++){
-            for(total_temp=0,i=0;i<3;i++){
-                total_temp+=table[i][j];
+    int i,j,k,sum=0;
+    int a[][2][3]={{{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}},{{13,14,15},{16,17,18}},{{19,20,21},{22,23,24}}};
+    for(i=0;i<4;i++){
+        for(j=0;j<2;j++){
+            for(k=0;k<3;k++){
+                sum+=a[i][j][k];
             }
-            avr_temp[j]=(total_temp/3);
-            printf("%5.1f\n",avr_temp[j]);
-
-    }
-    printf("------------------\n");
-
-    //時段一、時段二與時段三的平均氣溫
-    float dayavr_temp[3]={0},day_temp;
-    for(i=0;i<3;i++){
-        for(day_temp=0,j=0;j<4;j++){
-            day_temp+=table[i][j];
-        }
-        dayavr_temp[i]=(day_temp/3);
-        printf("%5.1f\n",dayavr_temp[i]);
-    }
-    printf("------------------\n");
-
-    //溫度最高的日子與時段
-    int time,day;
-    float hotest_time=dayavr_temp[0],hotest_day=avr_temp[0];
-    for(time=1,i=0;i<3;i++){
-        if(dayavr_temp[i]>hotest_time){
-            hotest_time=dayavr_temp[i];
-            time=i+1;
         }
     }
-    printf("the hotest time is %d\n",time);
-    for(day=1,j=0;j<4;j++){
-        if(avr_temp[j]>hotest_day){
-            hotest_day=avr_temp[j];
-            day=j+1;
-        }
-    }
-    printf("the hotest day is %d\n",day);
-    printf("------------------\n");
-    //溫度最低的日子與時段
-    int time2,day2;
-    float coldest_time=dayavr_temp[0],coldest_day=avr_temp[0];
-    for(time2=1,i=0;i<3;i++){
-        if(dayavr_temp[i]<coldest_time){
-            coldest_time=dayavr_temp[i];
-            time2=i+1;
-        }
-    }
-    printf("the coldest time=%d\n",time2);
-    for(day2=1,j=0;j<4;j++){
-        if(avr_temp[j]<coldest_day){
-            coldest_day=avr_temp[j];
-            day2=j+1;
-        }
-    }
-    printf("the coldest day=%d\n",day2);
+    printf("the sum of a[4][2][3]=%d\n",sum);
     return 0;
-
 }
 
 
