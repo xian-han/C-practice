5ebeb37c368f4aabbdda24be945eb10900d8971d
xianhan <xianhan0421@gmail.com>
2021-06-17 19:43:31 +0800

    c-practice ch9.3.22

--- main.c
+++ main.c
@@ -2,29 +2,38 @@
 #include <stdlib.h>
 #define COL 3
 #define ROW 2
-double average(int arr[ROW][COL]);
+void add(int A[ROW][COL],int B[ROW][COL],int C[ROW][COL]);
 int main(void)
 {
-    int a[ROW][COL]={{3,7,8},{4,3,9}};
+    int a[ROW][COL]={{3,7,8},{4,3,9}},b[][COL]={{1,7,4},{9,10,14}},c[][COL]={0};
     int i,j;
+    printf("a:\n");
     for(i=0;i<ROW;i++){
         for(j=0;j<COL;j++){
-            printf("%2d",a[i][j]);
+            printf("%3d",a[i][j]);
         }
         putchar('\n');
     }
-    printf("the average of a=%3.2lf\n",average(a));
-    return 0;
-}
-double average(int arr[ROW][COL]){
-    int i,j;
-    float sum=0;
+    printf("b:\n");
     for(i=0;i<ROW;i++){
         for(j=0;j<COL;j++){
-            sum+=arr[i][j];
+            printf("%3d",b[i][j]);
         }
+        putchar('\n');
     }
-    return sum/(ROW*COL);
+    add(a,b,c);
+    return 0;
+}
+void add(int A[ROW][COL],int B[ROW][COL],int C[ROW][COL]){
+    int i,j;
+    printf("---after adding---\n");
+    for(i=0;i<ROW;i++){
+        for(j=0;j<COL;j++){
+            C[i][j]=A[i][j]+B[i][j];
+            printf("%3d",C[i][j]);
+        }
+        putchar('\n');
+    }
 }
 
 
