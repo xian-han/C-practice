df7b1ffdb836fa6dfdda28db7c7679c90771daac
xianhan <xianhan0421@gmail.com>
2021-06-17 15:55:58 +0800

    c-practice ch9.3.20

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-void square(int arr[]);
+void count(int arr[]);
 int main(void)
 {
     int a[6]={3,6,2,5,4,1};
@@ -9,18 +9,35 @@ int main(void)
         printf("%3d",a[i]);
     }
     putchar('\n');
-    square(a);
-    //printf("the index of minimum of a=%2d\n",idx(a));
+    count(a);
     return 0;
 }
-void square(int arr[]){
-    int i;
-    printf("---after square()---\n");
+void count(int arr[]){
+    int i,odd_num=0,odd[6]={0},even_num=0,even[6]={0};
     for(i=0;i<6;i++){
-        arr[i]*=arr[i];
-        printf("%3d",arr[i]);
+        if(arr[i]%2==0){
+            even[even_num]=arr[i];
+            even_num++;
+        }
+        else{
+            odd[odd_num]=arr[i];
+            odd_num++;
+        }
     }
-    putchar('\n');
+    i=0;
+    printf("odd are:");
+    do{
+        printf("%2d",odd[i]);
+        i++;
+    }while(odd[i]!=0);
+    printf("\n the number of odd are %2d\n",odd_num);
+    i=0;
+    printf("even are:");
+    do{
+        printf("%2d",even[i]);
+        i++;
+    }while(even[i]!=0);
+    printf("\n the number of even are %2d\n",even_num);
 }
 
 
