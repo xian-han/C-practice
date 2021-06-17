001785b9b927ccb6b9570b18d1fa855efdc1b38d
xianhan <xianhan0421@gmail.com>
2021-06-17 19:52:18 +0800

    c-practice ch9.4.23

--- main.c
+++ main.c
@@ -1,40 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define COL 3
-#define ROW 2
-void add(int A[ROW][COL],int B[ROW][COL],int C[ROW][COL]);
 int main(void)
 {
-    int a[ROW][COL]={{3,7,8},{4,3,9}},b[][COL]={{1,7,4},{9,10,14}},c[][COL]={0};
-    int i,j;
-    printf("a:\n");
-    for(i=0;i<ROW;i++){
-        for(j=0;j<COL;j++){
-            printf("%3d",a[i][j]);
-        }
-        putchar('\n');
-    }
-    printf("b:\n");
-    for(i=0;i<ROW;i++){
-        for(j=0;j<COL;j++){
-            printf("%3d",b[i][j]);
-        }
-        putchar('\n');
-    }
-    add(a,b,c);
+    char str[]="Hello, C language";
+    printf("the size of str[]=%ld\n",sizeof(str));
     return 0;
 }
-void add(int A[ROW][COL],int B[ROW][COL],int C[ROW][COL]){
-    int i,j;
-    printf("---after adding---\n");
-    for(i=0;i<ROW;i++){
-        for(j=0;j<COL;j++){
-            C[i][j]=A[i][j]+B[i][j];
-            printf("%3d",C[i][j]);
-        }
-        putchar('\n');
-    }
-}
-
 
 
