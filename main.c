555a630d74b65320ee420c8ceeb95ba1a2280dc1
xianhan <xianhan0421@gmail.com>
2021-06-18 20:29:48 +0800

    c-practice ch9.5.27

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+void reverse(char str[]);
 int length(char str[]);
 int main(void)
 {
@@ -7,8 +8,19 @@ int main(void)
     int i=0;
     gets(str);
     fflush(stdin);
-    printf("the length of str=%d\n",length(str));
+    reverse(str);
+    puts(str);
     return 0;
+}
+void reverse(char str[]){
+    int leng,i;
+    char tem;
+    leng=length(str);
+    for(i=0;i<=(leng/2);i++){
+        tem=str[i];
+        str[i]=str[(leng-1)-i];
+        str[(leng-1)-i]=tem;
+    }
 }
 int length(char str[]){
     int leng=0,i=0;
