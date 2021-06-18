239b41d8ac69a3a8d3ec6baa9efd0e7803246631
xianhan <xianhan0421@gmail.com>
2021-06-18 20:06:06 +0800

    c-practice ch9.4.25

--- main.c
+++ main.c
@@ -3,53 +3,15 @@
 int main(void)
 {
     char str[100];
-    int time[5]={0},i=0,n=0;
+    int i=0;
     scanf("%s",&str);
     fflush(stdin);
     do{
-        switch(str[i]){
-        case('a'):
-            time[0]++;
-            break;
-        case('e'):
-            time[1]++;
-            break;
-        case('i'):
-            time[2]++;
-            break;
-        case('o'):
-            time[3]++;
-            break;
-        case('u'):
-            time[4]++;
-            break;
-        default:
-            break;
+        if(str[i]>='A'&&str[i]<='Z'){
+            str[i]+=32;
         }
         i++;
     }while(str[i]!='\0');
-    i=0;
-    do{
-        switch(i){
-        case(0):
-            printf("a:%d\n",time[0]);
-            break;
-        case(1):
-            printf("e:%d\n",time[1]);
-            break;
-        case(2):
-            printf("i:%d\n",time[2]);
-            break;
-        case(3):
-            printf("o:%d\n",time[3]);
-            break;
-        case(4):
-            printf("u:%d\n",time[4]);
-            break;
-        default:
-            break;
-        }
-        i++;
-    }while(i<5);
+    printf("%s\n",str);
     return 0;
 }
