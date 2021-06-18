b7a19a35375e9f56ca1d072ae2ee62e8370976d6
xianhan <xianhan0421@gmail.com>
2021-06-18 20:14:59 +0800

    c-practice ch9.5.26

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+int length(char str[]);
 int main(void)
 {
     char str[100];
     int i=0;
-    scanf("%s",&str);
+    gets(str);
     fflush(stdin);
+    printf("the length of str=%d\n",length(str));
+    return 0;
+}
+int length(char str[]){
+    int leng=0,i=0;
     do{
-        if(str[i]>='A'&&str[i]<='Z'){
-            str[i]+=32;
-        }
+        leng++;
         i++;
     }while(str[i]!='\0');
-    printf("%s\n",str);
-    return 0;
+    return leng;
 }
