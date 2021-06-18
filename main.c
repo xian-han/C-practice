a84837da94974333f29b179f7502c8153bc2b390
xianhan <xianhan0421@gmail.com>
2021-06-18 20:39:58 +0800

    c-practice ch9.5.28

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-void reverse(char str[]);
+void toLower(char str[]);
 int length(char str[]);
 int main(void)
 {
     char str[100];
-    int i=0;
     gets(str);
     fflush(stdin);
-    reverse(str);
+    toLower(str);
     puts(str);
     return 0;
 }
-void reverse(char str[]){
-    int leng,i;
-    char tem;
-    leng=length(str);
-    for(i=0;i<=(leng/2);i++){
-        tem=str[i];
-        str[i]=str[(leng-1)-i];
-        str[(leng-1)-i]=tem;
-    }
+void toLower(char str[]){
+    int i=0,leng=length(str);
+    do{
+        if(str[i]>='A'&&str[i]<='Z'){
+            str[i]+=32;
+        }
+        i++;
+    }while(i<leng);
 }
 int length(char str[]){
     int leng=0,i=0;
